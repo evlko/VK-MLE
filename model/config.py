c10d26e2a228a112479d01a29d4feb94ead6165a
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-03-09 11:55:45 +0300

    [add] model config file

---
+++ model/config.py
@@ -0,0 +1,18 @@
+# Features after EDA
+INPUT_SIZE = 14
+
+# Training hyperparameters
+LEARNING_RATE = 0.001
+BATCH_SIZE = 64
+MIN_EPOCHS = 1
+MAX_EPOCHS = 10
+
+# Dataset
+TRAIN_DATA_PATH = "data/custom_train_df.csv"
+TEST_DATA_PATH = "data/custom_test_df.csv"
+NUM_WORKERS = 4
+
+# Compute related
+ACCELERATOR = "gpu"
+DEVICES = [0]
+PRECISION = 16
