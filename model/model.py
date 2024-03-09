c5e0655e38e4ecb8f4ae958072088a2a31287aaf
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-03-09 23:27:51 +0300

    [refactor] ndcg scoring

--- model/model.py
+++ model/model.py
@@ -15,12 +15,9 @@ class RankingModel(pl.LightningModule):
         self.fc3 = nn.Linear(32, 1)
 
         self.loss_fn = F.binary_cross_entropy
-
-        self.validation_step_targets = []
-        self.validation_step_outputs = []
-
-        self.test_step_targets = []
-        self.test_step_outputs = []
+        
+        self.validation_scores_targets = []
+        self.test_scores_targets = []
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
@@ -33,6 +30,13 @@ class RankingModel(pl.LightningModule):
         outputs = self(inputs).squeeze(1)
         loss = self.loss_fn(outputs, targets.float())
         return loss, outputs, targets
+    
+    def _score_ndcg(self, scores_targets, log_name: str):
+        all_targets = torch.cat([item[0] for item in scores_targets]).cpu().numpy()
+        all_scores = torch.cat([item[1] for item in scores_targets]).cpu().numpy()
+        ndcg = ndcg_score(all_targets.reshape(1, -1), all_scores.reshape(1, -1))
+        self.log(log_name, ndcg)
+        scores_targets.clear()
 
     def training_step(self, batch, batch_idx):
         loss, scores, y = self._sharable_step(batch, batch_idx)
@@ -41,37 +45,21 @@ class RankingModel(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         loss, scores, y = self._sharable_step(batch, batch_idx)
-        self.validation_step_targets.append(y)
-        self.validation_step_outputs.append(scores)
+        self.validation_scores_targets.append((y, scores))
         self.log("val_loss", loss)
         return loss
 
     def on_validation_epoch_end(self):
-        all_preds = (
-            torch.cat([out for out in self.validation_step_outputs]).cpu().numpy()
-        )
-        all_targets = (
-            torch.cat([out for out in self.validation_step_targets]).cpu().numpy()
-        )
-        ndcg = ndcg_score(all_targets.reshape(1, -1), all_preds.reshape(1, -1))
-        self.log("val_ndcg", ndcg)
-        self.validation_step_outputs.clear()
-        self.validation_step_targets.clear()
+        self._score_ndcg(self.validation_scores_targets, "val_ndcg")
 
     def test_step(self, batch, batch_idx):
         loss, scores, y = self._sharable_step(batch, batch_idx)
-        self.test_step_targets.append(y)
-        self.test_step_outputs.append(scores)
+        self.test_scores_targets.append((y, scores))
         self.log("test_loss", loss)
         return loss
 
     def on_test_epoch_end(self):
-        all_preds = torch.cat([out for out in self.test_step_outputs]).cpu().numpy()
-        all_targets = torch.cat([out for out in self.test_step_targets]).cpu().numpy()
-        ndcg = ndcg_score(all_targets.reshape(1, -1), all_preds.reshape(1, -1))
-        self.log("test_ndcg", ndcg)
-        self.test_step_outputs.clear()
-        self.test_step_targets.clear()
+        self._score_ndcg(self.test_scores_targets, "test_ndcg")
 
     def predict_step(self, batch, batch_idx):
         inputs, targets = batch
