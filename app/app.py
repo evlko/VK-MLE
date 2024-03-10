e3a41338138497ff13f1219b355e36aa15f51439
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-03-10 17:38:16 +0300

    [add] api config and predictions endpoint

--- app/app.py
+++ app/app.py
@@ -1,18 +1,30 @@
+import json
+from typing import List
+
+import torch
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
+
+import app.config as config
 from app.schema import FeaturesData
-import torch
 
 app = FastAPI()
 
 
 def load_model():
-    return torch.jit.load("app/model.pt")
+    return torch.jit.load(config.MODEL_PATH)
+
+
+def load_features_info():
+    with open(config.FEATURES_INFO_PATH, "r") as file:
+        scaler_info = json.load(file)
+    return scaler_info
 
 
 model = load_model()
 model.eval()
+features_info = load_features_info()
 
 
 @app.get("/health")
@@ -23,14 +35,27 @@ def get_server_status():
 
 
 @app.post("/prediction")
-def get_prediction(features: FeaturesData) -> int:
+def get_prediction(objects: List[FeaturesData]) -> int:
     """Get prediction from loaded model based on"""
-    prediction = {"prediction": 1}
-    values = []
-    for _ in range(14):
-        values.append(1)
-    input_tensor = torch.tensor([values], dtype=torch.float32)
-    with torch.no_grad():
-        output = model(input_tensor)
-    print(output)
-    return JSONResponse(content=jsonable_encoder(prediction))
+    scores = []
+
+    for object in objects:
+        features = object.features
+        values = []
+        for feature in features_info["max"]:
+            if feature in features:
+                values.append(
+                    (features[feature] - features_info["min"][feature])
+                    / (features_info["max"][feature] - features_info["min"][feature])
+                )
+        input_tensor = torch.tensor([values], dtype=torch.float32)
+        try:
+            with torch.no_grad():
+                score = model(input_tensor)
+                scores.append(int(torch.squeeze(score)))
+        except RuntimeError as e:
+            scores.append(-1)
+
+    result = {"predictions": scores}
+
+    return JSONResponse(content=jsonable_encoder(result))
---
+++ app/config.py
@@ -0,0 +1,2 @@
+MODEL_PATH = "app/model.pt"
+FEATURES_INFO_PATH = "app/scaler_info.json"
